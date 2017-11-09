ba285772b33180a3e9670e9eebfec5cc9b3620fd
Jerraloid <jerryblaauw@hotmail.com>
2017-11-09 22:55:23 +0100

    baah

--- src/fiets/graphics/SpriteSheet.java
+++ src/fiets/graphics/SpriteSheet.java
@@ -21,7 +21,7 @@ public class SpriteSheet {
     public final int SIZE; //grootte van de spritesheet
     public int[] pixels;
     
-    public static SpriteSheet tiles = new SpriteSheet("/textures/spritesheet.png", 256);
+    public static SpriteSheet tiles = new SpriteSheet("/textures/sheets/spritesheet.png", 256);
     
     public SpriteSheet(String path, int size) {
         this.path = path; 
